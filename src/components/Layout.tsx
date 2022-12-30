c348a4b6741284ca1cd0493315cd85a70f39a681
roggc <roggc9@gmail.com>
2022-12-30 22:38:34 +0100

    change layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,31 @@
 import { Link, Outlet } from "@tanstack/react-router";
+import styled from "styled-components";
 
 const Layout = () => (
-  <>
-    <div>
+  <Container>
+    <LateralMenu>
       <Link to="/">Home</Link> <Link to="/dashboard">Dashboard</Link>
-    </div>
-    <hr />
-    <Outlet />
-  </>
+    </LateralMenu>
+    <StyledOutlet>
+      <Outlet />
+    </StyledOutlet>
+  </Container>
 );
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const LateralMenu = styled.div`
+  width: 180px;
+  display: flex;
+  flex-direction: column;
+  border-right: 1px solid ${({ theme }) => theme.colors.main};
+`;
+
+const StyledOutlet = styled.div`
+  flex-grow: 1;
+`;
 
 export default Layout;
