f0310b01446c3296fc915b6a81bcdabe509d98d5
roggc <roggc9@gmail.com>
2022-12-30 20:47:34 +0100

    use localhost api

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,17 @@
-const Dashboard = () => (
-  <div>
-    <h3>Dashboard</h3>
-  </div>
-);
+import { useEffect } from "react";
+import { API_URL } from "../constants";
+
+const Dashboard = () => {
+  useEffect(() => {
+    fetch(`http://${API_URL}/data?lat=33.44&lon=-94.04&exclude=hourly,daily`)
+      .then((resp) => resp.json())
+      .then((data) => console.log(data));
+  }, []);
+  return (
+    <div>
+      <h3>Dashboard</h3>
+    </div>
+  );
+};
+
 export default Dashboard;
