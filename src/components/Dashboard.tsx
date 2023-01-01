cb823a1e35344c99c4e56f826a523647e2f359e5
roggc <roggc9@gmail.com>
2023-01-01 13:11:59 +0100

    show chart

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,13 +1,49 @@
 import { useValues, data } from "slices";
 import { CURRENT, HISTORY } from "constants_";
+import { LineChart, Line, XAxis, YAxis, Tooltip, Label } from "recharts";
 
 const Dashboard = () => {
-  const { [HISTORY]: historyData } = useValues(data);
-  console.log("hystory data", historyData);
+  const {
+    [HISTORY]: {
+      data: historyData,
+      isLoading: historyIsLoading,
+      error: historyError,
+    },
+  } = useValues(data);
+
+  const isShowHistoryGraph = !historyIsLoading && !historyError;
+
+  if (isShowHistoryGraph) {
+    console.log("hystory data", historyData);
+  }
+
   return (
-    <div>
-      <h3>Dashboard</h3>
-    </div>
+    <>
+      {isShowHistoryGraph && (
+        <LineChart
+          width={300}
+          height={200}
+          data={historyData.hourly.map((ho: any, index: number) => ({
+            humidity: ho.humidity,
+            hour: index,
+          }))}
+        >
+          <Line dataKey="humidity" type="monotone" />
+          <XAxis dataKey="hour">
+            <Label value="hour" offset={0} position="insideBottom" />
+          </XAxis>
+          <YAxis>
+            <Label
+              value="humidity"
+              offset={0}
+              position="insideLeft"
+              angle={-90}
+            />
+          </YAxis>
+          <Tooltip />
+        </LineChart>
+      )}
+    </>
   );
 };
 
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => (
-  <div>
+  <>
     <h3>Weather forecast app</h3>
-  </div>
+  </>
 );
 export default Home;
