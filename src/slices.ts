809bd5cb966e7dc1013530fc9d12b2cc47e988a6
roggc <roggc9@gmail.com>
2023-06-27 14:49:48 +0200

    use rcs@9.0.8

--- src/slices.ts
+++ src/slices.ts
@@ -3,21 +3,23 @@ import getHookAndProviderFromSlices, {
 } from "react-context-slices";
 
 export const { useSlice, Provider } = getHookAndProviderFromSlices({
-  city: defineSlice<string>({ initialArg: "" }),
-  currentData: defineSlice<{}>({ initialArg: {} }),
-  history1Data: defineSlice<{}>({ initialArg: {} }),
-  history2Data: defineSlice<{}>({ initialArg: {} }),
-  history3Data: defineSlice<{}>({ initialArg: {} }),
-  history4Data: defineSlice<{}>({ initialArg: {} }),
-  history5Data: defineSlice<{}>({ initialArg: {} }),
-  firebaseData: defineSlice<{}>({ initialArg: {} }),
-  hereData: defineSlice<{}>({ initialArg: {} }),
-  dataKey: defineSlice<string>({ initialArg: "humidity" }),
-  firebase: defineSlice<any>({ initialArg: null }),
-  googleAccessToken: defineSlice<any>({ initialArg: null }),
-  theme: defineSlice<boolean>({
-    initialArg: true,
-    isGetInitialStateFromStorage: true,
-  }),
-  user: defineSlice<any>({ initialArg: null }),
+  slices: {
+    city: defineSlice<string>({ initialArg: "" }),
+    currentData: defineSlice<{}>({ initialArg: {} }),
+    history1Data: defineSlice<{}>({ initialArg: {} }),
+    history2Data: defineSlice<{}>({ initialArg: {} }),
+    history3Data: defineSlice<{}>({ initialArg: {} }),
+    history4Data: defineSlice<{}>({ initialArg: {} }),
+    history5Data: defineSlice<{}>({ initialArg: {} }),
+    firebaseData: defineSlice<{}>({ initialArg: {} }),
+    hereData: defineSlice<{}>({ initialArg: {} }),
+    dataKey: defineSlice<string>({ initialArg: "humidity" }),
+    firebase: defineSlice<any>({ initialArg: null }),
+    googleAccessToken: defineSlice<any>({ initialArg: null }),
+    theme: defineSlice<boolean>({
+      initialArg: true,
+      isGetInitialStateFromStorage: true,
+    }),
+    user: defineSlice<any>({ initialArg: null }),
+  },
 });
